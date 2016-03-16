49331071997fdda1fabdab1c530b821e4c3e7f05
vstanev1 <vstanev1@gmail.com>
2016-03-16 15:37:50 -0400

    a dano

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def result():
 
 # add a circle renderer with a size, color, and alpha
      p = figure()
-     p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+     p.circle([1,2], [3,4])
      script, div = components(p) 
  #  return render_template('graph.html', script=script, div=div)    
      return render_template('graph.html', script=script, div=div)
