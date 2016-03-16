926cbeb59553737a655c3972759a76c93cb586c9
vstanev1 <vstanev1@gmail.com>
2016-03-16 11:19:14 -0400

    nov html

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 import simplejson as json
-import numpy as np
 import datetime
 from datetime import datetime
 
@@ -37,26 +36,13 @@ def index():
       
 @app.route('/result' , methods=['POST'])
 def result():
-      stock_tick = 'GOOG' 
-      web_adr = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stock_tick + '.json'
-      r = requests.get(web_adr)
- #     json_data = r.json()
-      parsed_data = json.loads(r.text)
-      new_data = pd.DataFrame(parsed_data['dataset']['data'])
-        #parsed_data['dataset']['data']
+     # plot = figure()
+     # plot.circle([1,2], [3,4])
         
-        #calendar.day_name[datetime.strptime('01/26/2016', '%m/%d/%Y').date().weekday()]
-      datetime.strptime(new_data[0][0], '%Y-%m-%d').date()
-      f = lambda x: datetime.strptime(x, '%Y-%m-%d').date()
-      new_data['Date'] = new_data[0].map(f)
-        
-      plot = figure(title= 'simple figure', x_axis_type="datetime")
-      r = plot.line(new_data['Date'][1:30].values, new_data[1][1:30].values)
-        
-      html = file_html(plot, CDN, "my plot")
-      Html_file= open("templates/index3.html","w")
-      Html_file.write(html)
-      Html_file.close()
+     # html = file_html(plot, CDN, "my plot")
+     # Html_file= open("templates/index3.html","w")
+     # Html_file.write(html)
+     # Html_file.close()
       return render_template('index3.html')  
 
 if __name__ == '__main__':
