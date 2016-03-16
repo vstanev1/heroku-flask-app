bb37d5ac20187eef65a63caba42253e4b899cfe5
vstanev1 <vstanev1@gmail.com>
2016-03-16 08:24:00 -0400

    nov html

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app.vars={}
 
 @app.route('/')
 def main():
-  return redirect('/index' ,methods=['GET','POST'])
+  #return redirect('/index' ,methods=['GET','POST'])
+   return render_template('index.html' ,methods=['GET','POST'])  
 
 @app.route('/index')
 def index():
   if request.method == 'GET':  
-     return render_template('index2.html' ,methods=['GET','POST'])
+     return render_template('index2.html', methods=['GET','POST'])
   if request.method == 'POST':  
       app.vars['stock_tkr'] = request.form['stock_tkr']   
   
