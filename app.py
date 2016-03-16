86003942444e5d507ea992943a483e380898a950
vstanev1 <vstanev1@gmail.com>
2016-03-16 12:45:18 -0400

    nov html

--- app.py
+++ app.py
@@ -53,13 +53,13 @@ def result():
   
    
 def build_plot():   
-     output_file('plot.html', title='Plot')
-     x_data = np.arange(1, 101)
-     y_data = np.random.randint(0, 101, 100)
+    #output_file('plot.html', title='Plot')
+    #x_data = np.arange(1, 101)
+    #y_data = np.random.randint(0, 101, 100)
 
     # Create a line plot from our data.
 
-     line(x_data, y_data)
+    #line(x_data, y_data)
      
      #snippet = curplot().create_html_snippet(embed_base_url='../static/js/', embed_save_loc='./static/js')
      snippet =  'index2.html'
