f7032deb5f760e53e56cdf6cf7cca61afcb90874
vstanev1 <vstanev1@gmail.com>
2016-03-16 15:01:31 -0400

    a dano

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def result():
      p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
      script, div = components(p) 
  #  return render_template('graph.html', script=script, div=div)    
-     return render_template('index6.html', script=script, div=div)
+     return render_template('graph.html', script=script, div=div)
 
     # plot = figure()
      # plot.circle([1,2], [3,4])
