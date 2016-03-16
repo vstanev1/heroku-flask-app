348c7729873bb89ca7bb538ae5a12a4b0050de30
vstanev1 <vstanev1@gmail.com>
2016-03-16 08:40:41 -0400

    nov html

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ app = Flask(__name__)
 
 app.vars={}
 
-@app.route('/')
+@app.route('/' , methods=['GET','POST'])
 def main():
    #return redirect('/index' ,methods=['GET','POST'])
-   return render_template('index2.html')  
+   # return render_template('index2.html') 
+   if request.method == 'GET':  
+     return render_template('index2.html')
+   else:  
+      app.vars['stock_tkr'] = request.form['stock_tkr']   
+      return render_template('index.html') 
 
 @app.route('/index')
 def index():
   if request.method == 'GET':  
-     return render_template('index2.html', methods=['GET','POST'])
+     return render_template('index2.html')
   else:  
       app.vars['stock_tkr'] = request.form['stock_tkr']   
       return render_template('index.html' ,methods=['GET','POST'])
