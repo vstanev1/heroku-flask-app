3b57b7e0766c20e72c3aadf24e01edeca8abc762
vstanev1 <vstanev1@gmail.com>
2016-03-16 12:43:13 -0400

    nov html

--- app.py
+++ app.py
@@ -44,14 +44,29 @@ def result():
         
      # html = file_html(plot, CDN, "my plot")
      # Html_file= open("templates/index3.html","w")
-     # Html_file.write(html)
+     # Html_file.write(html)  
      # Html_file.close()
-     return render_template('index2html') 
-     #lot_snippet = build_plot()
-
+    #return render_template('index2.html)
+     plot_snippet = build_plot()
+     return render_template(plot_snippet)
     #return render_template('plots.html', snippet=plot_snippet)
-#   return render_template(plot_snippet) 
+  
    
+def build_plot():   
+     output_file('plot.html', title='Plot')
+     x_data = np.arange(1, 101)
+     y_data = np.random.randint(0, 101, 100)
+
+    # Create a line plot from our data.
+
+     line(x_data, y_data)
+     
+     #snippet = curplot().create_html_snippet(embed_base_url='../static/js/', embed_save_loc='./static/js')
+     snippet =  'index2.html'
+    # Return the snippet we want to place in our page.
+
+     return snippet
+
 
 if __name__ == '__main__':
   app.run(port=33507)
